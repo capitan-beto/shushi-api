4cd0d2068b4cc2344fbfa53cce16cef98446927c
Otroletravaladna <crnana98@gmail.com>
2023-08-09 00:39:57 -0300

    edit id type

--- src/main/java/com/capitanbeto/sushi/product/Product.java
+++ src/main/java/com/capitanbeto/sushi/product/Product.java
@@ -1,7 +1,7 @@
 package com.capitanbeto.sushi.product;
 
 public class Product {
-    private Long id;
+    private int id;
 
     private String name;
 
@@ -16,7 +16,7 @@ public class Product {
     public Product() {
     }
 
-    public Product(Long id, String name, float price, String category, String description, String image) {
+    public Product(int id, String name, float price, String category, String description, String image) {
         this.id = id;
         this.name = name;
         this.price = price;
@@ -30,6 +30,54 @@ public class Product {
         this.price = price;
         this.category = category;
         this.description = description;
+        this.image = image;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
         this.image = image;
     }
 }
