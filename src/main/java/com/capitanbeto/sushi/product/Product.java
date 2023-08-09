e186c1fccc92146007507152517f371814412182
Otroletravaladna <crnana98@gmail.com>
2023-08-09 00:23:21 -0300

    create product variables and constructors

--- src/main/java/com/capitanbeto/sushi/product/Product.java
+++ src/main/java/com/capitanbeto/sushi/product/Product.java
@@ -1,4 +1,35 @@
 package com.capitanbeto.sushi.product;
 
 public class Product {
+    private Long id;
+
+    private String name;
+
+    private float price;
+
+    private String category;
+
+    private String description;
+
+    private String image;
+
+    public Product() {
+    }
+
+    public Product(Long id, String name, float price, String category, String description, String image) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+        this.description = description;
+        this.image = image;
+    }
+
+    public Product(String name, float price, String category, String description, String image) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+        this.description = description;
+        this.image = image;
+    }
 }
