0eec000e38012f89e398a726585710b02649165a
Otroletravaladna <crnana98@gmail.com>
2023-08-09 00:54:25 -0300

    remove mapping and delete unused imports

--- src/main/java/com/capitanbeto/sushi/SushiApplication.java
+++ src/main/java/com/capitanbeto/sushi/SushiApplication.java
@@ -1,12 +1,7 @@
 package com.capitanbeto.sushi;
 
-import com.capitanbeto.sushi.product.Product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @SpringBootApplication
 public class SushiApplication {
@@ -14,19 +9,4 @@ public class SushiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SushiApplication.class, args);
 	}
-
-	@GetMapping(path = "/")
-	public List<Product> getNames() {
-		return List.of(
-		 new Product(
-				 1,
-				"Poshi",
-				500,
-				"Roll",
-				"Roll de pollo",
-				"imagen.com"
-		 )
-		);
-	}
-
 }
