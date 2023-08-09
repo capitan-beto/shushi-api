dbe3bb45680e3e1df46095e913e0d53c7e1d857a
Otroletravaladna <crnana98@gmail.com>
2023-08-09 00:42:31 -0300

    create product controller and migrate resource controller

--- src/main/java/com/capitanbeto/sushi/SushiApplication.java
+++ src/main/java/com/capitanbeto/sushi/SushiApplication.java
@@ -1,15 +1,32 @@
 package com.capitanbeto.sushi;
 
+import com.capitanbeto.sushi.product.Product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @SpringBootApplication
-@RestController	
 public class SushiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SushiApplication.class, args);
+	}
+
+	@GetMapping(path = "/")
+	public List<Product> getNames() {
+		return List.of(
+		 new Product(
+				 1,
+				"Poshi",
+				500,
+				"Roll",
+				"Roll de pollo",
+				"imagen.com"
+		 )
+		);
 	}
 
 }
