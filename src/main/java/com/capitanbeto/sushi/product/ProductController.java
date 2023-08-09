111bbea3846c96e47ee28983c491324438ea75fa
Otroletravaladna <crnana98@gmail.com>
2023-08-09 00:55:05 -0300

    set controller class and mapping annotations

--- src/main/java/com/capitanbeto/sushi/product/ProductController.java
+++ src/main/java/com/capitanbeto/sushi/product/ProductController.java
@@ -1,7 +1,26 @@
 package com.capitanbeto.sushi.product;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping(path = "/api/v1/products")
 public class ProductController {
+
+    @GetMapping
+    public List<Product> getProducts() {
+        return List.of(
+                new Product(
+                        1,
+                        "Poshi",
+                        500,
+                        "Roll",
+                        "Roll de pollo",
+                        "imagen.com"
+                )
+        );
+    }
 }
