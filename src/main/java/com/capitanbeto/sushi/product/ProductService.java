bedead5ddcd42208fb5c2302fb0b06d74a136bf8
Otroletravaladna <crnana98@gmail.com>
2023-08-13 23:32:46 -0300

    add setter DI

--- src/main/java/com/capitanbeto/sushi/product/ProductService.java
+++ src/main/java/com/capitanbeto/sushi/product/ProductService.java
@@ -1,5 +1,6 @@
 package com.capitanbeto.sushi.product;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -7,11 +8,18 @@ import java.util.List;
 @Service
 public class ProductService {
 
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
     public List<Product> getProducts() {
         return List.of(
                 new Product(
                         1,
-                        "Poshi",
+                        "Futurama",
                         500,
                         "Roll",
                         "Roll de pollo",
