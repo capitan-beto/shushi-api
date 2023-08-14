066f297a57d90277be1939a93a1b8d92d24e4425
Otroletravaladna <crnana98@gmail.com>
2023-08-13 23:04:31 -0300

    change id data type

--- src/main/java/com/capitanbeto/sushi/product/Product.java
+++ src/main/java/com/capitanbeto/sushi/product/Product.java
@@ -7,7 +7,7 @@ import jakarta.persistence.*;
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Integer id;
 
     private String name;
 
