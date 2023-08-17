6375d5da21a2723c19dbee37a69f225d593cf57c
Otroletravaladna <crnana98@gmail.com>
2023-08-17 18:36:50 -0300

    add request body annotation to addProduct parameter

--- src/main/java/com/capitanbeto/sushi/product/ProductController.java
+++ src/main/java/com/capitanbeto/sushi/product/ProductController.java
@@ -1,10 +1,7 @@
 package com.capitanbeto.sushi.product;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,7 +22,7 @@ public class ProductController {
     }
 
     @PostMapping
-    public void addProduct(Product product) {
+    public void addProduct(@RequestBody Product product) {
         this.productService.newProduct(product);
     }
 }
