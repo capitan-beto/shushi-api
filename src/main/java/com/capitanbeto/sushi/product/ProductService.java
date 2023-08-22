e6a848bec21578f198137716c352f26df691579d
Otroletravaladna <crnana98@gmail.com>
2023-08-21 22:49:41 -0300

    edit post request response

--- src/main/java/com/capitanbeto/sushi/product/ProductService.java
+++ src/main/java/com/capitanbeto/sushi/product/ProductService.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,15 +25,21 @@ public class ProductService {
 
     public ResponseEntity<Object> newProduct(Product product) {
         Optional<Product> res = productRepository.findProductByName(product.getName());
+        HashMap<String, Object> data = new HashMap<>();
 
         if (res.isPresent()) {
+            data.put("error", true);
+            data.put("message", "There's already a product with that name");
             return new ResponseEntity<>(
+                    data,
                     HttpStatus.CONFLICT
             );
         }
         productRepository.save(product);
+        data.put("data", product);
+        data.put("message", "Product successfully saved");
         return new ResponseEntity<>(
-                product,
+                data,
                 HttpStatus.CREATED
         );
     }
