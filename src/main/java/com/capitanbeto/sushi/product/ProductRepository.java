21b41fb34508ac8f7311a49aa55f24ceb519fde1
Otroletravaladna <crnana98@gmail.com>
2023-08-22 00:01:22 -0300

    Edit JPA repository 2nd argument type

--- src/main/java/com/capitanbeto/sushi/product/ProductRepository.java
+++ src/main/java/com/capitanbeto/sushi/product/ProductRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Integer> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 
     Optional<Product> findProductByName(String name);
 }
