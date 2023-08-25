217b2a2e4cfbf1c9f168b8ca7aa6650e4a42014a
Otroletravaladna <crnana98@gmail.com>
2023-08-25 14:46:20 -0300

    create app initial data file, edit init rules

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO product (price, category, description, image, name) VALUES (3500, "Combo Vegetariano", "45 piezas variadas aptas Vegetarianos", "www.imagen.com/comboveggie", "Combo x45 Veggie prueba");
+INSERT INTO product (price, category, description, image, name) VALUES (3500, "Combo Vegetariano", "45 piezas variadas aptas Vegetarianos", "www.imagen.com/comboveggie", "Combo x45 Veggie prueba 2");
