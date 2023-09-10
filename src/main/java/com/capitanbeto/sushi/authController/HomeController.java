1233c8e436ef26fac866617db0b8c382db62e598
Otroletravaladna <crnana98@gmail.com>
2023-09-10 02:36:14 -0300

    create auth controller package

---
+++ src/main/java/com/capitanbeto/sushi/authController/HomeController.java
@@ -0,0 +1,13 @@
+package com.capitanbeto.sushi.authController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping
+    public String home() {
+        return "Hello, Carlete";
+    }
+}
