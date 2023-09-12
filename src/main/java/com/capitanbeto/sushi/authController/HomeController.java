ef44bc913b7f2954507c656900740ccf8d3828d2
Otroletravaladna <crnana98@gmail.com>
2023-09-11 23:05:38 -0300

    edit home mapping content

--- src/main/java/com/capitanbeto/sushi/authController/HomeController.java
+++ src/main/java/com/capitanbeto/sushi/authController/HomeController.java
@@ -3,11 +3,13 @@ package com.capitanbeto.sushi.authController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController
 public class HomeController {
 
     @GetMapping
-    public String home() {
-        return "Hello, Carlete";
+    public String home(Principal principal) {
+        return "Hello " + principal.getName();
     }
 }
