c3ed683fcb77f940768f80a8a65ee0498afc29ad
Otroletravaladna <crnana98@gmail.com>
2023-09-15 21:21:01 -0300

    add resource server config

--- src/main/java/com/capitanbeto/sushi/config/SecurityConfig.java
+++ src/main/java/com/capitanbeto/sushi/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -33,6 +34,8 @@ public class SecurityConfig {
                 .authorizeRequests(auth -> auth
                         .anyRequest().authenticated()
                 )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(Customizer.withDefaults()))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .httpBasic(withDefaults())
                 .build();
