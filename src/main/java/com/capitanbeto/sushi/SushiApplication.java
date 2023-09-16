bfd1bcc7984e7c4436fe04df6116cf54a1cfbe1a
Otroletravaladna <crnana98@gmail.com>
2023-09-15 21:57:09 -0300

    externalize RSA keys

--- src/main/java/com/capitanbeto/sushi/SushiApplication.java
+++ src/main/java/com/capitanbeto/sushi/SushiApplication.java
@@ -1,8 +1,11 @@
 package com.capitanbeto.sushi;
 
+import com.capitanbeto.sushi.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(RsaKeyProperties.class)
 @SpringBootApplication
 public class SushiApplication {
 
