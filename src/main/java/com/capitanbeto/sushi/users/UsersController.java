21f396d2700ed79ab5e127ae8085c1d27e1d8f87
Otroletravaladna <crnana98@gmail.com>
2023-09-19 00:40:04 -0300

    create and test controller

---
+++ src/main/java/com/capitanbeto/sushi/users/UsersController.java
@@ -0,0 +1,15 @@
+package com.capitanbeto.sushi.users;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1/users")
+public class UsersController {
+
+    @GetMapping
+    public String testController() {
+        return "Hola bebeto";
+    }
+}
