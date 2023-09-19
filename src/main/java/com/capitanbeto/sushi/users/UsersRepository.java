feaa14557ed5fb54dc173696283ce075668a86fe
Otroletravaladna <crnana98@gmail.com>
2023-09-19 00:40:33 -0300

    create users repository

---
+++ src/main/java/com/capitanbeto/sushi/users/UsersRepository.java
@@ -0,0 +1,8 @@
+package com.capitanbeto.sushi.users;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsersRepository extends JpaRepository<User, Long> {
+}
