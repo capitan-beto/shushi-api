bf9d57a1191286f3db0f491345a94881cb0fbf1c
Otroletravaladna <crnana98@gmail.com>
2023-09-19 01:10:26 -0300

    inject service layer, add users get method map

--- src/main/java/com/capitanbeto/sushi/users/UsersController.java
+++ src/main/java/com/capitanbeto/sushi/users/UsersController.java
@@ -4,12 +4,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(path = "/api/v1/users")
 public class UsersController {
 
+    private final UsersService usersService;
+
+    public UsersController(UsersService usersService) {
+        this.usersService = usersService;
+    }
+
     @GetMapping
-    public String testController() {
-        return "Hola bebeto";
+    public List<Users> getUsers() {
+        return usersService.getUsers();
     }
 }
