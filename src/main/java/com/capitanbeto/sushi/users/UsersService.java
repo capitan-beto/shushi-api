2212e42b7746e460048a75d08650a1a735705ce4
Otroletravaladna <crnana98@gmail.com>
2023-09-19 00:43:14 -0300

    create users service layer

---
+++ src/main/java/com/capitanbeto/sushi/users/UsersService.java
@@ -0,0 +1,15 @@
+package com.capitanbeto.sushi.users;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsersService {
+
+    private final UsersRepository usersRepository;
+
+    @Autowired
+    public UsersService(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+}
