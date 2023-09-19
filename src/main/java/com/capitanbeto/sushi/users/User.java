9464453538125eddb72753829d8f1195e588a351
Otroletravaladna <crnana98@gmail.com>
2023-09-19 00:32:36 -0300

    set User class

--- src/main/java/com/capitanbeto/sushi/users/User.java
+++ src/main/java/com/capitanbeto/sushi/users/User.java
@@ -1,5 +1,77 @@
 package com.capitanbeto.sushi.users;
 
+import jakarta.persistence.*;
 
+@Entity
+@Table
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    private String phone;
+
+    public User(Long id, String email, String username, String password, String phone) {
+        this.id = id;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.phone = phone;
+    }
+
+
+    public User(String email, String username, String password, String phone) {
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.phone = phone;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
 }
