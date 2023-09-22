7f74264d365e2317e83bb9b35b8eaa9da281982f
Otroletravaladna <crnana98@gmail.com>
2023-09-22 18:59:12 -0300

    add get and post mapping

--- src/main/java/com/capitanbeto/sushi/users/UserController.java
+++ src/main/java/com/capitanbeto/sushi/users/UserController.java
@@ -1,6 +1,5 @@
 package com.capitanbeto.sushi.users;
 
-import com.capitanbeto.sushi.product.Product;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,21 +7,21 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/v1/users")
-public class UsersController {
+public class UserController {
 
-    private final UsersService usersService;
+    private final UserService userService;
 
-    public UsersController(UsersService usersService) {
-        this.usersService = usersService;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
     @GetMapping
     public List<User> getUsers() {
-        return usersService.getUsers();
+        return userService.getUsers();
     }
 
     @PostMapping
     public ResponseEntity<Object> addUser(@RequestBody User user) {
-        return this.usersService.newUser(user);
+        return this.userService.newUser(user);
     }
 }
