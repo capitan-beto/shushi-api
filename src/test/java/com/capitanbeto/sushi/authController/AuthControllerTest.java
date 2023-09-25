2bdc27c7abd0c3901c2cd5726597baf75e31809b
Otroletravaladna <crnana98@gmail.com>
2023-09-25 04:09:57 -0300

    create auth controller test suite and first test

---
+++ src/test/java/com/capitanbeto/sushi/authController/AuthControllerTest.java
@@ -0,0 +1,29 @@
+package com.capitanbeto.sushi.authController;
+
+import com.capitanbeto.sushi.config.SecurityConfig;
+import com.capitanbeto.sushi.service.TokenService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest({AuthController.class})
+@Import({SecurityConfig.class, TokenService.class})
+class AuthControllerTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    void RootWhenAuthenticatedThen401() throws Exception {
+        this.mvc.perform(get("/token"))
+                .andExpect(status().isUnauthorized());
+
+    }
+
+}
