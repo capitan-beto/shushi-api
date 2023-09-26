ef3fb39e276fdcee886cc9420b28f37b4788bf15
Otroletravaladna <crnana98@gmail.com>
2023-09-25 22:39:07 -0300

    create first test case

--- src/test/java/com/capitanbeto/sushi/product/ProductControllerTest.java
+++ src/test/java/com/capitanbeto/sushi/product/ProductControllerTest.java
@@ -1,18 +1,45 @@
 package com.capitanbeto.sushi.product;
 
+import com.capitanbeto.sushi.config.SecurityConfig;
+import com.capitanbeto.sushi.service.TokenService;
+import com.jayway.jsonpath.JsonPath;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest({ProductController.class})
 @ExtendWith({SpringExtension.class})
+@Import({SecurityConfig.class, TokenService.class})
 class ProductControllerTest {
 
     @Autowired
     MockMvc mvc;
 
+    @MockBean
+    ProductService service;
+
+    @Test
+    void testProducts() throws Exception {
+        mvc.perform(get("/api/v1/products")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
 }
