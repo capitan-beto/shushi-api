dd82e003726bccdd20840f65368ec688341de216
Otroletravaladna <crnana98@gmail.com>
2023-09-26 00:54:53 -0300

    tests: add test for single product fetch

--- src/test/java/com/capitanbeto/sushi/product/ProductControllerTest.java
+++ src/test/java/com/capitanbeto/sushi/product/ProductControllerTest.java
@@ -12,7 +12,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -20,8 +22,13 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Objects;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.*;
@@ -60,5 +67,13 @@ class ProductControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(1)))
                 .andExpect(jsonPath("$[0].name", is("Full Salmon X45")));
+    }
+
+    @Test
+    void getSingleProduct() throws Exception {
+        when(service.getSingleProducts(1L)).thenReturn(new ResponseEntity<Object>(products, HttpStatus.OK) );
+        MvcResult result = mvc.perform(get("/api/v1/products/1"))
+                .andExpect(status().isOk()).andReturn();
+        assertThat("Full Salmon X45", result.getResponse().getContentAsString().contains("Full Salmon X45"));
     }
 }
