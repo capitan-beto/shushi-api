c21a26501903046baca6691024bc454005a4a539
Otroletravaladna <crnana98@gmail.com>
2023-09-26 00:28:18 -0300

    tests: refactor first test case, mock products list

--- src/test/java/com/capitanbeto/sushi/product/ProductControllerTest.java
+++ src/test/java/com/capitanbeto/sushi/product/ProductControllerTest.java
@@ -3,6 +3,7 @@ package com.capitanbeto.sushi.product;
 import com.capitanbeto.sushi.config.SecurityConfig;
 import com.capitanbeto.sushi.service.TokenService;
 import com.jayway.jsonpath.JsonPath;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,8 +20,12 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.Collections;
+
 import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -36,10 +41,24 @@ class ProductControllerTest {
     @MockBean
     ProductService service;
 
+    private Product products;
+
+    @BeforeEach
+    public void setup() {
+        products = new Product(1L,
+                "Full Salmon X45",
+                13,
+                "combos",
+                "45 pieces of Sake, Cream Roll, Salmon Maki, Feel Roll, Tropical, SPF, Geisha, Nigiri, Sashimi",
+                "www.imagen.com");
+    }
+
     @Test
     void testProducts() throws Exception {
-        mvc.perform(get("/api/v1/products")
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+        when(service.getProducts()).thenReturn(Collections.singletonList(products));
+        mvc.perform(get("/api/v1/products"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].name", is("Full Salmon X45")));
     }
 }
