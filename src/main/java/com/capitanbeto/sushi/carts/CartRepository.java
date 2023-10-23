61da138c816e13ab7ae2bf33a5b15a8d194a435a
Otroletravaladna <crnana98@gmail.com>
2023-10-22 23:07:59 -0300

    create cart repository

--- src/main/java/com/capitanbeto/sushi/carts/CartRepository.java
+++ src/main/java/com/capitanbeto/sushi/carts/CartRepository.java
@@ -1,4 +1,6 @@
 package com.capitanbeto.sushi.carts;
 
-public interface CartRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartRepository extends JpaRepository<Cart, Long> {
 }
