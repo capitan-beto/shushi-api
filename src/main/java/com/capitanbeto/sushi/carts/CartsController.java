b356aa03697557e8fd2a1d3925f48fdf8faef3d1
Otroletravaladna <crnana98@gmail.com>
2023-10-22 23:04:35 -0300

    create carts controller

---
+++ src/main/java/com/capitanbeto/sushi/carts/CartsController.java
@@ -0,0 +1,9 @@
+package com.capitanbeto.sushi.carts;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1/carts")
+public class CartsController {
+}
