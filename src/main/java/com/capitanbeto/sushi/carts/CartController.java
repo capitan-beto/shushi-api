18527b1eccd49bbc576007728aef10ff7aadd266
Otroletravaladna <crnana98@gmail.com>
2023-10-22 23:24:42 -0300

    add get mapping to controller

--- src/main/java/com/capitanbeto/sushi/carts/CartController.java
+++ src/main/java/com/capitanbeto/sushi/carts/CartController.java
@@ -1,9 +1,25 @@
 package com.capitanbeto.sushi.carts;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/carts")
 public class CartController {
+
+    private final CartService cartService;
+
+    public CartController(CartService cartService) {
+        this.cartService = cartService;
+    }
+
+    @GetMapping({"", "/"})
+    public List<Cart> getCarts() {
+        return this.cartService.getCarts();
+    }
+
+
 }
