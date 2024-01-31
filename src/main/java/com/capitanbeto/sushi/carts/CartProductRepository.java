739028228f88208017577755db6afe42237a00ef
Otroletravaladna <crnana98@gmail.com>
2024-01-31 00:11:20 -0300

    feat: create cart products repo

---
+++ src/main/java/com/capitanbeto/sushi/carts/CartProductRepository.java
@@ -0,0 +1,9 @@
+package com.capitanbeto.sushi.carts;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartProductRepository extends JpaRepository<CartProduct, Long> {
+}
