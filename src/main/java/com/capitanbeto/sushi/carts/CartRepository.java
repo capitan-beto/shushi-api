e235e1fbe5322d83c30e4c10634dc5568a113a9b
Otroletravaladna <crnana98@gmail.com>
2024-01-31 01:02:38 -0300

    feat: create find by id method in repository

--- src/main/java/com/capitanbeto/sushi/carts/CartRepository.java
+++ src/main/java/com/capitanbeto/sushi/carts/CartRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 public interface CartRepository extends JpaRepository<Cart, Long> {
 
     Optional<Cart> findCartByUserId(Long id);
+    Optional<Cart> findById(Long id);
 }
